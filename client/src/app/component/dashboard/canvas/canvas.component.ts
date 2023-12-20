0d1a0af8e44adebeafb9beb7295b73196f370043
Pranay Varma <pranayvarma@Pranays-MacBook-Air.local>
2023-12-21 00:03:09 +0530

    added a canvas

--- client/src/app/component/dashboard/canvas/canvas.component.ts
+++ client/src/app/component/dashboard/canvas/canvas.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss']
 })
-export class CanvasComponent {
-
+export class CanvasComponent implements OnInit {
+  ngOnInit(): void {
+    var c = document.getElementById("myCanvas") as HTMLCanvasElement;
+    var ctx = c.getContext("2d");
+    if (ctx) {
+      ctx.moveTo(500,0);
+      ctx.lineTo(0,500);
+      ctx.stroke();
+    } else {
+      console.error("Unable to get 2D context for canvas");
+    }
+  }
 }
