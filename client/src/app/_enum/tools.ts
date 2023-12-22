f78455a8426d8606215c16ef72785ad5d5018e9a
Pranay Varma <pranayvarma@Pranays-MacBook-Air.local>
2023-12-22 21:04:03 +0530

    enum and model specified

---
+++ client/src/app/_enum/tools.ts
@@ -0,0 +1,9 @@
+export enum Tools {
+    FREE_DRAW = 'FREE_DRAW',
+    CIRCLE = 'CIRCLE',
+    SQUARE = 'SQUARE',
+    RECTANGLE = 'RECTANGLE',
+    DIAMOND = 'DIAMOND',
+    UNI_ARROW = 'UNI_ARROW',
+    BI_ARROW = 'BI_ARROW'
+}
--- client/src/app/component/dashboard/canvas/canvas.component.ts
+++ client/src/app/component/dashboard/canvas/canvas.component.ts
@@ -1,71 +1,83 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
+import {Canvas} from '../../../_model/canvas'
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss']
 })
 export class CanvasComponent implements OnInit {
+  public canvas!: HTMLCanvasElement
+  public canvasHeight!: number;
+  public canvasWidth!: number;
+  public canvasClass: Canvas = new Canvas(this.canvas, this.canvasHeight, this.canvasWidth);
+  public ctx!: CanvasRenderingContext2D | null;
+
+  public isDrawing: boolean = false;
+
+
+  public toggleTool = [
+    { name: 'freeDraw', enum: 'FREE_DRAW', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: true },
+    { name: 'circle', enum: 'CIRCLE', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: false },
+    { name: 'square', enum: 'SQUARE', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: false },
+    { name: 'rectangle', enum: 'RECTANGLE', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: false },
+    { name: 'diamond', enum: 'DIAMOND', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: false },
+    { name: 'uniArrow', enum: 'UNI_ARROW', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: false },
+    { name: 'biArrow', enum: 'BI_ARROW', color: '#000000', thickness: 2, hasFill: false, opacity: 1, isActive: false }
+  ];
+  
+  constructor(){
+    
+  }
   ngOnInit(): void {
-    // var c = document.getElementById("myCanvas") as HTMLCanvasElement;
-    // var width = window.innerWidth
-    // var height = window.innerHeight
-    // c.style.width = width.toString() + "px";
-    // c.style.height = height.toString() + "px"
-    // var ctx = c.getContext("2d");
-    // var cty = c.getContext("2d");
-    // if (ctx && cty) {
-    //   ctx.moveTo(0,0);
-    //   ctx.lineTo(c.width,c.height);
-    //   cty.moveTo(c.width,0);
-    //   cty.lineTo(0,c.height); 
-    //   ctx.stroke();
-    //   cty.stroke();
+      this.setCanvas();
+  }
+
+  setCanvas(): void {
+    this.canvasClass.canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+    this.canvasClass.canvasHeight = window.innerHeight;
+    this.canvasClass.canvasWidth = window.innerWidth;
+
+    this.canvasClass.canvas.width = this.canvasClass.canvasWidth;
+    this.canvasClass.canvas.height = this.canvasClass.canvasHeight;
+
+    this.ctx = this.canvasClass.canvas.getContext("2d");
+  } 
+
+  freeDraw(): void {
+
+  }
+  blash(): void {
+    // if (this.ctx) {
+    //   this.canvasClass.canvas.addEventListener("mousedown", function (e: MouseEvent) {
+    //       if(this.ctx) {
+    //         this.isDrawing = true;
+    //         ctx.beginPath();
+    //         ctx.moveTo(offsetX, offsetY);
+    //         console.log("init x",e.offsetX,"init y", e.offsetY)
+    //       }
+    //     });
+    
+    //     c.addEventListener("mousemove", function (e: MouseEvent) {
+    //         if (isDrawing && ctx) {
+    //             ctx.lineTo(e.clientX + e.offsetX, e.clientY + e.offsetY);
+    //             ctx.stroke();
+    //         }
+    //     });
+    
+    //     c.addEventListener("mouseup", function (event) {
+    //         isDrawing = false;
+    //         var finalX = event.offsetX;
+    //         var finalY = event.offsetY;
+    //         console.log("final x",event.offsetX,"final y", event.offsetY)
+    //     });
+    
+    //     c.addEventListener("mouseleave", function () {
+    //         isDrawing = false;
+    //     });
+    //     ctx.stroke();
     // } else {
-    //   console.error("Unable to get 2D context for canvas");
+    //     console.error("Unable to get 2D context for canvas");
     // }
-
-    // listen to mouse events:
-    // tracks coordinates of mouse
-    // set the initial coordinates before drag starts, to ctx.moveTo()
-    // set the final coordinates after drag ends, to ctx.lineTo()
-
-    var c = document.getElementById("myCanvas") as HTMLCanvasElement;
-    c.style.width = window.innerWidth.toString() + "px";
-    c.style.height = window.innerHeight.toString() + "px";
-    var ctx = c.getContext("2d");
-    var isDrawing = false;
-    if (ctx) {
-        c.addEventListener("mousedown", function (event) {
-          if(ctx) {
-            isDrawing = true;
-            ctx.beginPath();
-            ctx.moveTo(event.offsetX, event.offsetY);
-            console.log("init x",event.offsetX,"init y", event.offsetY)
-          }
-        });
     
-        c.addEventListener("mousemove", function (event) {
-            if (isDrawing && ctx) {
-                ctx.lineTo(event.offsetX, event.offsetY);
-                ctx.stroke();
-            }
-        });
-    
-        c.addEventListener("mouseup", function (event) {
-            isDrawing = false;
-            var finalX = event.offsetX;
-            var finalY = event.offsetY;
-            console.log("final x",event.offsetX,"final y", event.offsetY)
-        });
-    
-        c.addEventListener("mouseleave", function () {
-            isDrawing = false;
-        });
-        ctx.stroke();
-    } else {
-        console.error("Unable to get 2D context for canvas");
-    }
-    
-      }
+  }
 }
